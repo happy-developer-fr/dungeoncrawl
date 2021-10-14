9e0e471c751195eb432f857a6eec8e4510d6cda7
cottet.julien <julien.cottet@preligens.com>
2021-10-14 17:22:06 +0200

    ✨ Random dungeon generation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod map;
 mod player;
+mod map_builder;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -9,6 +10,7 @@ mod prelude {
 
     pub use crate::map::*;
     pub use crate::player::*;
+    pub use crate::map_builder::*;
 }
 
 use prelude::*;
@@ -20,9 +22,11 @@ struct State {
 
 impl State {
     fn new() -> Self {
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
         Self {
-            map: Map::new(),
-            player: Player::new(Point::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
         }
     }
 }
@@ -30,6 +34,12 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
         self.player.update(ctx, &self.map);
         self.map.render(ctx);
         self.player.render(ctx);
